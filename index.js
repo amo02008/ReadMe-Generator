1ee70aff491674e8901d3a5e1a7f3d0e1ae67d1d
amo02008 <adrienne.osorio@gmail.com>
2021-03-14 20:41:58 -0400

    cleaned up functions on index page

---
+++ index.js
@@ -0,0 +1,84 @@
+//Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Questions for user to complete
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "GitHub Username:",
+    },
+    {
+      type: "input",
+      name: "emailAddress",
+      message: "What is your email address?",
+    },
+
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What would you like your README title to be?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Write a description of your project",
+    },
+    {
+      type: "input",
+      name: "installationInstructions",
+      message: "What are the dependencies needed to run your project",
+    },
+    {
+      type: "input",
+      name: "usageInforamtion",
+      message: "Provide Instructions and examples for use",
+    },
+    {
+      type: "input",
+      name: "contributionGuidelines",
+      message: "Guidelines for how others should contribute to your project",
+    },
+    {
+      type: "input",
+      name: "testInstructions",
+      message: "Tests and examples of how to run them",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "License Type",
+      choices: [
+        "MIT",
+        "GNU GPL v3.0",
+        "GNU AGPL v3.0",
+        "BSD 2-Clause",
+        "BSD 3-Clause",
+        "No license",
+      ],
+    },
+  ]);
+};
+
+const init = async () => {
+  const answers = await promptUser();
+  const markdown = generateMarkdown(answers);
+  
+  await writeFileAsync("DemoREADME.md", markdown);
+  console.log("Successfully wrote README");
+};
+
+init();
+
+//code from class
+
+const init = async () => {
+  const answers = await promptUser();
+  const markdown = generateMarkdown(answers);
+};
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,96 @@
+const badgeLicense = renderLicenseBadge(answers.license);
+    const linkLicense = renderLicenseLink(answers.license);
+    const sectionLicense = renderLicenseSection(answers.license);
+
+
+function renderLicenseBadge(license) {
+    switch (license) {
+      case "MIT":
+        return "[![GitHub license](https://img.shields.io/badge/license-MIT-brightgreen)](https://img.shields.io/badge/license-MIT-brightgreen)";
+      case "GNU GPL v3.0":
+        return "[![GitHub license](https://img.shields.io/badge/license-GNU_GPL_v3.0-brightgreen)](https://img.shields.io/badge/license-GNU_GPL_v3.0-brightgreen)";
+      case "GNU AGPL v3.0":
+        return "[![GitHub license](https://img.shields.io/badge/license-GNU_AGPL_v3.0-brightgreen)](https://img.shields.io/badge/license-GNU_AGPL_v3.0-brightgreen)";
+      case "BSD 2-Clause":
+        return "[![GitHub license](https://img.shields.io/badge/license-BSD_2_Clause-brightgreen)](https://img.shields.io/badge/license-BSD_2_Clause-brightgreen)";
+      case "BSD 3-Clause":
+        return "[![GitHub license](https://img.shields.io/badge/license-BSD_3_Clause-brightgreen)](https://img.shields.io/badge/license-BSD_3_Clause-brightgreen)";
+      case "No license":
+        return "";
+    }
+  }
+  
+function renderLicenseLink(license) {
+    switch (license) {
+      case "MIT":
+        return "https://choosealicense.com/licenses/mit/";
+      case "GNU GPL v3.0":
+        return "https://choosealicense.com/licenses/gpl-3.0";
+      case "GNU AGPL v3.0":
+        return "https://choosealicense.com/licenses/agpl-3.0/";
+      case "BSD 2-Clause":
+        return "https://choosealicense.com/licenses/bsd-2-clause";
+      case "BSD 3-Clause":
+        return "https://choosealicense.com/licenses/bsd-3-clause";
+      case "No license":
+        return "";
+    }
+  }
+
+
+  
+    
+  function renderLicenseSection(license) {
+    if(license === "No license") {
+      return "";
+    } else {
+      return `
+    ## License
+      
+    This project has been created under the ${answers.license} license.`;
+    }
+  };
+
+
+
+  const generateMarkdown = (answers) => {    
+
+   return ` ${answers.projectTitle}
+
+${renderLicenseBadge(answers.license)}
+
+## Description
+${answers.description}
+
+## Table of Contents
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+## Installation Instructions
+${answers.installationInstructions}
+
+## Usage Inforamtion
+${answers.usageInforamtion}
+${renderLicenseSection}
+
+##Contribution Guidelines
+${answers.contributionGuidelines}
+
+## Test Instructions
+${answers.testInstructions}
+
+## GitHub Username
+${answers.username}
+
+##Questions
+Please email me with any questions at ${answers.emailAddress}`
+}
+  
+  
+  module.exports = generateMarkdown;
